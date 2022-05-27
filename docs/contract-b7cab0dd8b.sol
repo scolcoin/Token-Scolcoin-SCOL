01e4da009c4cd36ba3a89635e0eefeaa672ae945
Social Coin of Colombia (SCOL) <33010917+scolcoin@users.noreply.github.com>
2022-05-27 16:45:29 -0500

    Contrato Scolcoin
    Dirección contrató 0x0E22277E5d1c9122a9deEefC0bd84f656daaEB16 en segunda Capa

---
+++ docs/contract-b7cab0dd8b.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+
+contract Scolcoin is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Scolcoin", "SCOL") {
+        _mint(msg.sender, 1 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
